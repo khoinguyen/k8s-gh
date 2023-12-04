92598bff958077df2af74d91dec05d51b953436b
Khoi Nguyen <khoi.nqq@gmail.com>
2023-12-04 19:22:32 +1100

    fix: remove support arm

--- init.d/01-hello.sh
+++ init.d/01-hello.sh
@@ -1,1 +1,4 @@
-gh workflow list --repo khoinguyen/k8s-gh
+NAMESPACE="default"
+kubectl get deployments -o name -n $NAMESPACE | xargs -I {} kubectl rollout status {} -n $NAMESPACE 
+kubectl get jobs -o name -n $NAMESPACE | xargs -I {} kubectl wait --for=condition=complete {} -n $NAMESPACE
+gh workflow list --repo $GH_OWNER/$GH_REPO
